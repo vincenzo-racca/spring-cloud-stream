86b90ec223f3481fc7c1e6123f5b2cabf94e7a0e
vincenzo.racca <vincenzo.racca22@gmail.com>
2023-06-12 18:24:36 +0200

    Revert "functions concat in imperative mode"
    This reverts commit 6e9fca07dd03615dd1b0524b446c88785502b7ed.

--- src/main/java/com/vincenzoracca/springcloudstream/event/SensorEventImperativeFunctions.java
+++ src/main/java/com/vincenzoracca/springcloudstream/event/SensorEventImperativeFunctions.java
@@ -11,7 +11,6 @@ import org.springframework.context.annotation.Configuration;
 
 import java.time.Instant;
 import java.util.function.Consumer;
-import java.util.function.Function;
 import java.util.function.Supplier;
 
 /**
@@ -31,17 +30,11 @@ public class SensorEventImperativeFunctions {
         return () -> new SensorEventMessage("2", Instant.now(), 30.0);
     }
 
-    @Bean
-    public Function<SensorEventMessage, SensorEventMessage> logEventReceived() {
-        return sensorEventMessage ->  {
-            log.info("Message received: {}", sensorEventMessage);
-            return sensorEventMessage;
-        };
-    }
 
     @Bean
-    public Consumer<SensorEventMessage> saveInDBEventReceived() {
+    public Consumer<SensorEventMessage> logEventReceivedSaveInDBEventReceived() {
         return sensorEventMessage ->  {
+            log.info("Message received: {}", sensorEventMessage);
             sensorEventDao.save(Mono.just(sensorEventMessage)).block();
         };
     }
